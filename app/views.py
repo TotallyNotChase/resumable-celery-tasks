b32256d95dad3ef7a6e4470e866c7b66e34f0969
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-12-01 22:54:58 +0630

    Route for resuming task

--- app/views.py
+++ app/views.py
@@ -1,12 +1,22 @@
+import json
+
 from app import app
-from app.tasks import add, check, mult
+from app.tasks import add, check, mult, save_state
 from app.tappable import tappable
+from app.utils import deserialize_chain
 
 tappable_operation = tappable(
-        (add.s(6) | mult.s(16)), check.s()
-    )
+    (add.s(6) | mult.s(16)), check.s(), save_state.s()
+)
 
 @app.route('/')
 def index():
-    print(tappable_operation(4))
+    tappable_operation(4)
     return 'hello world'
+
+@app.route('/resume')
+def resume():
+    with open('chains.json') as f:
+        t_chain = json.load(f)
+    deserialize_chain(t_chain)(10)
+    return 'on it'
