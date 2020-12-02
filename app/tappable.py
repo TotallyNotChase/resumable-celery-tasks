d27455dcf4bc3a30a79153e3dde8fe8ada239603
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-12-02 23:37:52 +0300

    Formatting pass

--- app/tappable.py
+++ app/tappable.py
@@ -3,15 +3,19 @@ from typing import Any, Optional
 from celery import shared_task
 from celery.canvas import Signature, chain, signature
 
+
 @shared_task(bind=True)
-def cancel(self, retval: Optional[Any]=None, clause: dict=None, callback: dict=None):
+def cancel(
+    self, retval: Optional[Any] = None, clause: dict = None, callback: dict = None
+):
     if signature(clause)(retval):
         signature(callback)(retval, self.request.chain)
         self.request.chain = None
     else:
         return retval
 
-def tappable(ch: chain, clause: Signature, callback: Signature, nth: Optional[int]=1):
+
+def tappable(ch: chain, clause: Signature, callback: Signature, nth: Optional[int] = 1):
     newch = []
     for n, sig in enumerate(ch.tasks):
         if n != 0 and n % nth == nth - 1:
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='resumable-celery-tasks',
-    version='0.1.0',
-    url='https://github.com/TotallyNotChase/resumable-celery-tasks',
-    license='MIT',
-    author='TotallyNotChase',
-    author_email='totallynotchase42@gmail.com',
+    name="resumable-celery-tasks",
+    version="0.1.0",
+    url="https://github.com/TotallyNotChase/resumable-celery-tasks",
+    license="MIT",
+    author="TotallyNotChase",
+    author_email="totallynotchase42@gmail.com",
     description='Demonstration of "resumable tasks" with celery',
 )
