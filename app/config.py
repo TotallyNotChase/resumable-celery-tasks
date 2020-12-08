0643ca83c613e1ea40e41c1771dc3d11037265ff
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-12-09 00:14:53 +0600

    Add cleanup schedule + capitalize config names

--- app/config.py
+++ app/config.py
@@ -1,11 +1,15 @@
-import os
-
-# Config file for flask app
-
 # Celery config keys
-CELERY_broker_url = os.environ["CELERY_BROKER_URL"]
-CELERY_result_backend = os.environ["CELERY_RESULT_BACKEND"]
-# Make celery propagate exceptions instead of retrying
-CELERY_task_eager_propagates = True
 
-# 'amqp://atlanuser:atlanpass@localhost:5672/atlanvhost'
+# Make celery propagate exceptions instead of retrying
+CELERY_TASK_EAGER_PROPAGATES = True
+# Set expiry on task results (in seconds)
+CELERY_RESULT_EXPIRES = 5
+# Set the `backend_cleanup` task to run every 5 minutes
+CELERY_BEAT_SCHEDULE = {
+    "backend_cleanup": {
+        "task": "celery.backend_cleanup",
+        "schedule": 5.0 * 60.0,
+    },
+}
+# Route the backend_cleanup task to a separate queue
+CELERY_TASK_ROUTES = {"celery.backend_cleanup": {"queue": "periodic_cleanup"}}
